4723776c0b78f3d2c676b7fb3c4c4a2f7afadd7b
Johana Fernandez <najofe5@gmail.com>
2025-02-21 16:28:18 +0100

    feat: Ajustes de estilos buscador

--- src/components/SearchBar/SearchBar.tsx
+++ src/components/SearchBar/SearchBar.tsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import { TextField, InputAdornment } from '@mui/material';
 import SearchIcon from '@mui/icons-material/Search';
+import "./SearchBar.css"; 
 
 interface SearchBarProps {
   value: string;
@@ -10,7 +11,7 @@ interface SearchBarProps {
 export const SearchBar: React.FC<SearchBarProps> = ({ value, onChange }) => {
   return (
     <TextField
-      fullWidth
+      className='search-container'
       variant="outlined"
       placeholder="Busca por nombre"
       value={value}
@@ -23,5 +24,6 @@ export const SearchBar: React.FC<SearchBarProps> = ({ value, onChange }) => {
         ),
       }}
     />
+
   );
 };
